321134f8a6ea20a4f92cfe802f9af1286ed73784
Laurent Peuch <cortex@worlddomination.be>
2019-04-30 16:41:06 +0200

    [enh] relase 0.1.1 with screenshot and README for pypi

--- setup.py
+++ setup.py
@@ -5,13 +5,14 @@ from setuptools import setup
 
 
 setup(name="pytest-capture-deprecatedwarnings",
-      version='0.1',
+      version='0.1.1',
       author='Laurent Peuch',
       author_email='cortex@worlddomination.be',
       url='https://github.com/psycojoker/pytest-capture-deprecatedwarnings',
       description='pytest plugin to capture all deprecatedwarnings and put them in one file',
-      # long_description=open("README.md").read(),
-      license="gplv3+",
+      long_description=open("README.md").read(),
+      long_description_content_type='text/markdown',
+      license="MIT",
       packages=['pytest_capture_deprecatedwarnings'],
       entry_points={'pytest11': ['pytest_capture_deprecatedwarnings = pytest_capture_deprecatedwarnings']},
       install_requires=['pytest'],
